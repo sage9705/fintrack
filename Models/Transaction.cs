571c47ece274e03f6c78d57eae72fcacdbfb1652
Sage9705 <edem22kumahor@gmail.com>
2024-01-03 11:45:00 +0000

    Create Transaction model

---
+++ Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FinTrack.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsIncome { get; set; }
+    }
+}
