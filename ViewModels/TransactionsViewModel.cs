4dcb049017f12a220ec6d082848ba824484fbbe2
Sage9705 <edem22kumahor@gmail.com>
2024-01-04 14:45:00 +0000

    Create TransactionsViewModel

---
+++ ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FinTrack.Models;
+using FinTrack.Services;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace FinTrack.ViewModels
+{
+    public partial class TransactionsViewModel : ObservableObject
+    {
+        private readonly ITransactionService _transactionService;
+
+        [ObservableProperty]
+        private ObservableCollection<Transaction> transactions;
+
+        public TransactionsViewModel(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+            Transactions = new ObservableCollection<Transaction>();
+        }
+
+        [RelayCommand]
+        private async Task LoadTransactionsAsync()
+        {
+            var loadedTransactions = await _transactionService.GetTransactionsAsync();
+            Transactions.Clear();
+            foreach (var transaction in loadedTransactions)
+            {
+                Transactions.Add(transaction);
+            }
+        }
+    }
+}
