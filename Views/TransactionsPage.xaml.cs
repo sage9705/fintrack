7296fd42aa049de09ce02264f127532380610d39
Sage9705 <edem22kumahor@gmail.com>
2024-01-05 11:30:00 +0000

    Create TransactionsPage with basic UI

---
+++ Views/TransactionsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FinTrack.ViewModels;
+
+namespace FinTrack.Views
+{
+    public partial class TransactionsPage : ContentPage
+    {
+        public TransactionsPage(TransactionsViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await ((TransactionsViewModel)BindingContext).LoadTransactionsCommand.ExecuteAsync(null);
+        }
+    }
+}
