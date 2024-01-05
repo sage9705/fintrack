fb909ccfdef39c6ba0a656550666fbe51bec3808
Sage9705 <edem22kumahor@gmail.com>
2024-01-05 15:00:00 +0000

    Set MainPage to AppShell in App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,11 @@
-﻿namespace FinTrack
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace FinTrack;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
     }
 }
