07ec561bba7c228a0a041b28506b2b9dcb276b1d
Sage9705 <edem22kumahor@gmail.com>
2024-01-06 11:00:00 +0000

    Create AddTransactionPage

---
+++ Views/AddTransactionPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using FinTrack.ViewModels;
+
+namespace FinTrack.Views
+{
+    public partial class AddTransactionPage : ContentPage
+    {
+        public AddTransactionPage(AddTransactionViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
