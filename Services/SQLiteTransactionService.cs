8a33fe677e65d89a678ee5def323c1d19c9df47a
Sage9705 <edem22kumahor@gmail.com>
2024-01-07 11:30:00 +0000

    Create SQLiteTransactionService

---
+++ Services/SQLiteTransactionService.cs
@@ -0,0 +1,42 @@
+﻿using FinTrack.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FinTrack.Services
+{
+    public class SQLiteTransactionService : ITransactionService
+    {
+        private readonly DatabaseContext _context;
+
+        public SQLiteTransactionService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Transaction>> GetTransactionsAsync()
+        {
+            return await _context.GetTransactionsAsync();
+        }
+
+        public async Task<Transaction> AddTransactionAsync(Transaction transaction)
+        {
+            await _context.SaveTransactionAsync(transaction);
+            return transaction;
+        }
+
+        public async Task<Transaction> UpdateTransactionAsync(Transaction transaction)
+        {
+            await _context.SaveTransactionAsync(transaction);
+            return transaction;
+        }
+
+        public async Task DeleteTransactionAsync(int id)
+        {
+            var transaction = await _context.GetTransactionAsync(id);
+            if (transaction != null)
+            {
+                await _context.DeleteTransactionAsync(transaction);
+            }
+        }
+    }
+}
