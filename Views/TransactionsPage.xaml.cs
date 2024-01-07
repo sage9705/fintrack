6966a02d587b601d9cb87f5cdc0bcc65b8b4bb10
Sage9705 <edem22kumahor@gmail.com>
2024-01-07 14:00:00 +0000

    Update TransactionsPage to pass ViewModel to AddTransactionPage

--- Views/TransactionsPage.xaml.cs
+++ Views/TransactionsPage.xaml.cs
@@ -4,16 +4,28 @@ namespace FinTrack.Views
 {
     public partial class TransactionsPage : ContentPage
     {
+        private readonly TransactionsViewModel _viewModel;
+
         public TransactionsPage(TransactionsViewModel viewModel)
         {
             InitializeComponent();
             BindingContext = viewModel;
+            _viewModel = viewModel;
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            await ((TransactionsViewModel)BindingContext).LoadTransactionsCommand.ExecuteAsync(null);
+            await _viewModel.LoadTransactionsCommand.ExecuteAsync(null);
+        }
+
+        private async void OnAddTransactionClicked(object sender, EventArgs e)
+        {
+            var navigationParameter = new Dictionary<string, object>
+            {
+                { "TransactionsViewModel", _viewModel }
+            };
+            await Shell.Current.GoToAsync("AddTransaction", navigationParameter);
         }
     }
 }
